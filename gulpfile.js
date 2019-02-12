fa0906d5ce8695b3b3f9e0acf2d685631fd99e2c
Breno Lemos <brenomiros@gmail.com>
2019-02-12 18:36:33 -0300

    Retira util do flow

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,10 @@
 const gulp = require('gulp');
-const util = require('gulp-util');
 const sequence = require('run-sequence');
 
 require('./gulpTasks/app');
 require('./gulpTasks/deps');
 
 gulp.task('default', () => {
-    if (util.env.production) {
-        sequence('deps', 'app');
-        // gulp.start('deps', 'app');
-    } else {
-        sequence('deps', 'app', 'monitorarMudancas');
-        // gulp.start('deps', 'app', 'servidor');
-    }
+    sequence('deps', 'app', 'monitorarMudancas');
+    // gulp.start('deps', 'app', 'servidor');
 });
